dcd6c60a650b1303823f896116fc02501532bb49
priyanshoe <priyanshurahate@gmail.com>
2026-02-07 14:35:12 +0530

    fixed bug

--- src/db/connection.js
+++ src/db/connection.js
@@ -7,7 +7,8 @@ try {
     port: process.env.PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+     ssl: { rejectUnauthorized: false } 
   });
 
   db.connect((err) => {
