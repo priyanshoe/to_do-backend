f6cf26726a4e4debba7b749da9330752e9a0c457
priyanshoe <priyanshurahate@gmail.com>
2026-08-15 18:38:17 +0530

    fixed bug

--- src/main/java/com/practice/todo/TodoApplication.java
+++ src/main/java/com/practice/todo/TodoApplication.java
@@ -1,9 +1,15 @@
 package com.practice.todo;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.todo.DTO.ApiResponse;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -18,7 +24,7 @@ public class TodoApplication {
 @RestController
 class InitialCheck{
 	@GetMapping("health")
-	public String checkServer() {
-		return "Server is running";
+	public ResponseEntity<ApiResponse> checkServer() {
+		return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("server is running", null));
 	}
 }
